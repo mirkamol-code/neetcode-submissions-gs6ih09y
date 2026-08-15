2f4e751f0d3d88ed102ca92a15be2996de1df388
mirkamol-code <mirkamol.m.a@gmail.com>
2026-08-15 16:13:53 +0500

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] counts = {0, 0, 0};
+
+        for(int index : nums){
+            counts[index] += 1;
+        }
+
+
+        int i = 0;
+        for(int n = 0; n < counts.length; n++){
+            for(int j = 0; j < counts[n]; j++){
+                nums[i] = n;
+                i++;
+            }
+        }
+    }
+}
